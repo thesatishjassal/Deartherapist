278c07130f36257c65cfe385b3df1240f0a1b43b
unknown <thesatishjassal@gmail.com>
2024-06-12 18:12:29 +0530

    uicode

--- app/page.js
+++ app/page.js
@@ -1,12 +1,48 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 import Image from "next/image";
-import styles from "./page.module.css";
+import Box from "@mui/material/Box";
 
-export default function Home() {
+
+export default function LoginForm() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-
-      </div>
-    </main>
+    <Card className="login__card">
+      <CardContent>
+        <Image src="/images/login-logo.png" width="150" height="100" />
+        <Typography gutterBottom variant="h5" className="mb-4 text-center">
+          Sign In
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-error-helper-text"
+            label="Email"
+            fullWidth
+            size="small"
+           variant="outlined" 
+          />
+          <TextField
+            id="outlined-error-helper-text"
+            label="Password"
+            fullWidth
+            size="small"
+           variant="outlined" 
+          />
+          <Button variant="contained" className="btn" size="medium">
+            Login
+          </Button>
+        </Box>
+      </CardContent>
+    </Card>
   );
 }
