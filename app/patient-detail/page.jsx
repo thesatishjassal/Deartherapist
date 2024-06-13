448eea3ffbd14a1243f174d23049c77467219d5d
unknown <thesatishjassal@gmail.com>
2024-06-13 15:14:16 +0530

    login card center align

--- app/patient-detail/page.jsx
+++ app/patient-detail/page.jsx
@@ -21,7 +21,7 @@ import { styled, alpha } from "@mui/material/styles";
 import { Container } from "@mui/material";
 import Button from "@mui/material/Button";
 import DownloadIcon from "@mui/icons-material/Download";
-import AddPrescription from "../components/addprescription"
+import AddPrescription from "../components/AddPrescription"
 
 const InvoicePage = () => {
   const invoiceRef = useRef();
