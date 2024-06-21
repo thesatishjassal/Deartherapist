186530772bf3039bdd230a53b90d475b85036eb4
unknown <thesatishjassal@gmail.com>
2024-06-22 01:35:57 +0530

    issue solve

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -20,13 +20,12 @@ const useAuth = () => {
         setUser(decoded);
       } catch (error) {
         console.error('Error decoding token:', error);
-        handleLogout(); // Handle token errors by logging out
+        handleLogout(); // Handle token decoding errors by logging out
       }
     } else {
       setUser(null); // Ensure user state is cleared if no token found
-      router.push('/'); // Redirect to login page if no token found
     }
-  }, []);
+  }, []); // Empty dependency array ensures useEffect runs only once on mount
 
   return { user, handleLogout };
 };
