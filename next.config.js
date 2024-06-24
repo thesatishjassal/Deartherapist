8165e6f219055170c9ba75c406eb15796d42ea81
unknown <thesatishjassal@gmail.com>
2024-06-24 13:19:27 +0530

    url updated for api

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['https://flagcdn.com'],
+    domains: ['flagcdn.com'],
   },
   env: {
     API_URL: 'https://api.deartherapist.in',
