4ec042d5741dc4c122702029e94743731c3fa8be
unknown <thesatishjassal@gmail.com>
2024-06-25 16:54:04 +0530

    proxy added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,28 +2,27 @@
 
 import React, { useState } from "react";
 import { Container, Grid } from "@mui/material";
-import useProtectedRoute from "../../hooks/useProtectedRoute";
+// import useProtectedRoute from "../../hooks/useProtectedRoute";
 import ChangePasswordForm from "../components/UserProfile"; // Adjust the path if necessary
 import CounselorChangePasswordForm from "../components/CounselorChangePasswordForm";
 import ReceptionistChangePasswordForm from "../components/ReceptionistChangePasswordForm";
 import useAuth from "../../hooks/useAuth"; // Adjust the import path as needed
 
 const ProfilePage = () => {
-  const user = useProtectedRoute();
-  const { myuser, handleLogout } = useAuth();
+  // const user = useProtectedRoute();
+  const { user, handleLogout } = useAuth();
 
   if (!user) {
     return null; // Optionally render a loading state or a redirect message
   }
-
   return (
     <Container sx={{ marginTop: "2rem" }}>
-      {myuser && myuser.role == "admin" ? (
+      {user && user.role == "admin" ? (
         <ChangePasswordForm />
-      ) : myuser.role == "counselor" ? (
+      ) : user.role == "counselor" ? (
         <CounselorChangePasswordForm />
       ) : (
-        (myuser.role = "recptionist" ? (
+        (user.role = "recptionist" ? (
           <ReceptionistChangePasswordForm />
         ) : (
           ""
