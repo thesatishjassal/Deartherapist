149f15eb96e3e60e2d493e723afb755ff02a9889
unknown <thesatishjassal@gmail.com>
2024-06-25 16:47:13 +0530

    proxy added

--- app/report/page.jsx
+++ app/report/page.jsx
@@ -24,12 +24,8 @@ import MenuItem from "@mui/material/MenuItem";
 import Container from '@mui/material/Container';
 
 const DailyReport = () => {
-  const user = useProtectedRoute();
-
-  if (!user) {
-    return null; // Optionally render a loading state or a redirect message
-  }  const invoiceRef = React.useRef();
-  
+  const user = useProtectedRoute();  
+  const invoiceRef = React.useRef();
   // State variables
   const [rows, setRows] = React.useState([]);
   const [filteredAppointments, setFilteredAppointments] = React.useState([]);
@@ -137,6 +133,11 @@ const DailyReport = () => {
     },
   });
 
+  
+  if (!user) {
+    return null; // Optionally render a loading state or a redirect message
+  }  
+  
   if (clientsError) {
     return <Typography variant="h6">Error loading clients data.</Typography>;
   }
