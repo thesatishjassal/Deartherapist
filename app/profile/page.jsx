57a435b88b7df1d8764cf58643e6e3d0016a5442
unknown <thesatishjassal@gmail.com>
2024-06-26 18:23:16 +0530

    yes commit

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
 import { Container, Grid } from "@mui/material";
 import ChangePasswordForm from "../components/UserProfile"; // Adjust the path if necessary
 import CounselorChangePasswordForm from "../components/CounselorChangePasswordForm";
