f5751b8faef542e801125f2ea8eceb88721913d7
unknown <thesatishjassal@gmail.com>
2024-06-26 18:24:42 +0530

    yes commit

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, {useEffect} from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
